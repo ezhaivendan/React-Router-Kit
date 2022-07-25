85ce3e2c68321ee75e2ba844c5448f171e5b4e75
ezhaivendan <ezhaivendan@gmail.com>
2022-07-25 22:31:25 +0530

    App Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Link, Switch } from 'react-router-dom';
+
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <>
+        <section>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Sales</Link>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <Switch>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/contact" component={Contact}></Route>
+            <Route path="/about" component={About}></Route>
+        </Switch>
+    </>
+  );
+}
+
+export default App;
